460560404b4357b019a49996bc0900556a574619
Ivan Belokobylskiy <belokobylskij@gmail.com>
2017-02-06 16:52:05 +0300

    PEP8 fixed

--- django_pickling.py
+++ django_pickling.py
@@ -1,12 +1,11 @@
-VERSION = (0, 2)
-__version__ = '.'.join(map(str, VERSION))
-
-
 from django.db.models import Model
 try:
     from itertools import izip
 except ImportError:
     izip = zip
+
+VERSION = (0, 2)
+__version__ = '.'.join(map(str, VERSION))
 
 
 def model_is_deferred(model):
@@ -23,23 +22,27 @@ def attnames(cls, _cache={}):
         _cache[cls] = [f.attname for f in cls._meta.fields]
         return _cache[cls]
 
+
 def model_unpickle(cls, data):
     obj = cls.__new__(cls)
     obj.__dict__.update(izip(attnames(cls), data))
     return obj
+
+
 model_unpickle.__safe_for_unpickle__ = True
 
-def Model__reduce__(self):
+
+def model__reduce__(self):
     if model_is_deferred(self):
-        return original_Model__reduce__(self)
+        return original_model__reduce__(self)
     else:
         cls = self.__class__
         data = self.__dict__.copy()
 
         vector = map(data.pop, attnames(cls))
-        return (model_unpickle, (cls, vector), data)
+        return model_unpickle, (cls, vector), data
 
-if Model.__reduce__ != Model__reduce__:
-    original_Model__reduce__ = Model.__reduce__
-    Model.__reduce__ = Model__reduce__
 
+if Model.__reduce__ != model__reduce__:
+    original_model__reduce__ = Model.__reduce__
+    Model.__reduce__ = model__reduce__
